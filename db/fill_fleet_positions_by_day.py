624a101615fb3a0009b49b3c6d61d5ee4670b4d5
pmarquis <pier.marquis@gmail.com>
2015-03-30 17:36:04 +0200

    erased comments in a python file

--- db/fill_fleet_positions_by_day.py
+++ db/fill_fleet_positions_by_day.py
@@ -2,7 +2,6 @@ from db import Database
 
 DATA_SOURCE_FILE = "data/positions_by_day.txt"
 
-
 INSERT_QUERY = """
 INSERT INTO fleet_positions_by_day (car_id, day, geometry)
     VALUES ({car_id}, now(), ST_GeomFromText('LINESTRING({polyline})', 4326));
@@ -15,7 +14,6 @@ class FleetPositionsByDay(Database):
         db.execute_file("sql/create_fleet_positions_by_day.sql")
 
     def fill_table(self):
-        # polyline = "37.78928902868515 -122.4123596110534,37.78979774729768 -122.41158713485709"
         coordinates = []
         with open(DATA_SOURCE_FILE, 'r') as positions_file:
             coordinates = positions_file.readlines()
